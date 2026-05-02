f973202d716b6b67428b055ea0b920299b53d4a2
JustBrownKid3252004 <phyoewaiheim@gmail.com>
2026-05-02 13:01:07 +0630

    Delete all_in_one directory

--- all_in_one/android/app/src/main/kotlin/com/example/all_in_one/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.all_in_one
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- all_in_one/lib/logic/navigation/navigation_cubit.dart
+++
@@ -1,9 +0,0 @@
-import 'package:bloc/bloc.dart';
-
-class NavigationCubit extends Cubit<int> {
-  NavigationCubit() : super(0);
-
-  void changeTab(int index) {
-    emit(index);
-  }
-}
